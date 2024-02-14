c7c4e8a32cd5b5f8d666731b4ccd958de8d1502f
soominnnn <marisol03@naver.com>
2024-02-15 03:35:37 +0900

    Setting: 프로젝트 생성과 환경설정 (#1)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-blue-600">
+        test
+      </h1>
+    </div>
+  );
+}
+
+export default App;
